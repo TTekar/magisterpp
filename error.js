2727b48de865e992db8ca635eab45e99ee5fa529
Tekar <101474437+TTekar@users.noreply.github.com>
2024-01-23 19:17:44 +0100

    fixed error page reload?

--- error.js
+++ error.js
@@ -1,15 +1,18 @@
 const init = function() {
+    window.location.replace(window.location.href.substring(0, window.location.href.lastIndexOf("/")));
+    //window.location.reload()
+
     const podium = document.getElementsByClassName("podium");
-    const img = podium[0].children[0].children[0];
-    img.src = "https://sundayguardianlive.com/wp-content/uploads/2023/07/Untitled-design-12.jpg";
-    // podium[0].firstChild().appendChild(img);
+    //const img = podium[0].children[0].children[0];
+    //img.src = "https://sundayguardianlive.com/wp-content/uploads/2023/07/Untitled-design-12.jpg";
+    //podium[0].firstChild().appendChild(img);
     const textDiv = podium[0].children[1];
     textDiv.className = "goaway";
 
-    const newText = document.createElement("h3");
-    newText.innerHTML = '"get better" -gesus';
-    newText.className = "getBetter";
-    podium[0].children[0].appendChild(newText);
+    //const newText = document.createElement("h3");
+    //newText.innerHTML = "nope";
+    //newText.className = "getBetter";
+    //podium[0].children[0].appendChild(newText);
 
 }
 
