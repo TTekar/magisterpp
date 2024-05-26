a8f588140a9916b2049ffe77e69af08af59c9317
Tekar <101474437+TTekar@users.noreply.github.com>
2024-05-26 23:04:31 +0200

    keuzekaart werkt soortvan

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ iframe.src = "https://www.jordanmlu.nl/agenda/";
 iframe.width = "100%";
 iframe.height = "100%";
 
-
+var hiddenUI = false;
 
 const init3 = function() {
 
@@ -25,9 +25,10 @@ const init3 = function() {
 
     // div1.appendChild(iframe);
 
-    
+  
     
     setTimeout(() => {
+      // Set custom pfp
         const divUserMenu = document.querySelector("a#user-menu");
         var pfp = divUserMenu.querySelector("figure img");
 
@@ -37,6 +38,65 @@ const init3 = function() {
         }else if(pfp.getAttribute("alt") == "Joppe Tummers") {
             pfp.setAttribute("src", "https://i.kym-cdn.com/photos/images/newsfeed/002/652/421/280.jpg")
         }
+
+
+        // Keuzemap button
+
+        // Keuze page
+        const mainView = document.querySelector("div.view.ng-scope")
+        const coverDiv = document.createElement("div")
+
+        coverDiv.width = "100vw"
+        coverDiv.height = "100vh"
+        coverDiv.id = "coverDiv"
+        coverDiv.style.display = "none"
+        mainView.parentElement.appendChild(coverDiv)
+
+        const iframeKeuze = document.createElement("iframe")
+        iframeKeuze.src = "https://jordanmlu.netlify.app/keuze"
+        iframeKeuze.style.width = "75vw"
+        iframeKeuze.style.height = "75vh"
+        coverDiv.appendChild(iframeKeuze)
+
+        // Button
+
+        const buttonsSideList = document.querySelector("ul.main-menu");
+        const newButtonList = document.createElement("li");
+        buttonsSideList.appendChild(newButtonList)
+
+        const newButton = document.createElement("a")
+        newButton.innerHTML = `<i class="far ng-scope fa-question" ng-if="item.icon" ng-class="item.icon"></i> <span ng-bind="item.title" class="caption ng-binding ng-scope" title="" ng-if="item.title !== 'OPP' &amp;&amp; item.title !== 'ELO'">Keuze Plattegrond</span>`
+        
+        //newButton.href = "https://jordanmlu.netlify.app/keuze"
+        newButton.href = "#"
+        newButton.classList.add("customButton")
+        newButton.onclick = function(event) {
+          event.preventDefault(); // Prevent the default action (navigation)
+          hiddenUI = true;
+      };
+      
+      newButtonList.appendChild(newButton);
+
+      const buttonsInListA = buttonsSideList.querySelectorAll("li a")
+
+      for (const link of buttonsInListA) {
+        if (!link.classList.contains("customButton")) {
+          link.onclick = function(event) {
+            event.preventDefault();
+            hiddenUI = false;
+          }
+        }
+        
+      }
+
+      // buttonsInListA.forEach((link, index) => {
+      //   if (index < links.length - 1) { // Exclude the last <a> element
+      //       link.addEventListener("click", () => {
+      //         console.log("ahhh")
+      //       } );
+      //   }
+      // });
+
     }, 1000);
 
     // setTimeout(() => {
@@ -78,6 +138,18 @@ var checkYoBadCijfers = window.setInterval(function(){
       document.getElementById("edit-toggle-btn").innerHTML = '<dna-icon name="far-pencil"></dna-icon><button aria-hidden="true" style="display: none" tabindex="-1" type="button"></button>';
     }catch {
       
+    }
+
+    // Check for hidden ui shit
+    const divToHide = document.querySelector("div.view.ng-scope")
+    const coverDiv = document.getElementById("coverDiv")
+
+    if (hiddenUI) {
+      divToHide.style.display = "none"
+      coverDiv.style.display = "block"
+    }else {
+      divToHide.style.display = "block"
+      coverDiv.style.display = "none"
     }
   
   }, 100);
